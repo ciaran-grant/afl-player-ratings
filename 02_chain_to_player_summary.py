9c4f1a8b40fd69bc726331f65871be8c4b2abb6c
Ciaran Grant <ciaran.grant@hotmail.co.uk>
2023-08-22 08:35:02 +1000

    scoring and merging scripts set up

---
+++ 02_chain_to_player_summary.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+
+chains = pd.read_csv("/Users/ciaran/Documents/Projects/AFL/data/scored_match_chains.csv")
+player_stats = pd.read_csv("/Users/ciaran/Documents/Projects/AFL/data/player_stats.csv")
+
+def get_player_totals_by_match(chains):
+    return chains.groupby(['Match_ID', 'Player', 'Team'])[['xScore', 'xT_created', 'xT_denied', 'vaep_value', 'offensive_value', 'defensive_value', 'exp_vaep_value', 'exp_offensive_value', 'exp_defensive_value']].sum().reset_index().sort_values(by=['Match_ID', "Player", "Team"])
+
+player_value = get_player_totals_by_match(chains)
+player_stats = player_stats.merge(player_value, how = "left", on = ['Match_ID', 'Player', 'Team'])
+
+def get_receiver_totals_by_match(chains):
+    
+    return chains.groupby(['Match_ID', 'Receiver'])[['xT_created', 'xT_denied', 'vaep_value', 'offensive_value', 'defensive_value', 'exp_vaep_value', 'exp_offensive_value', 'exp_defensive_value']].sum().reset_index().sort_values(by=['Match_ID', "Receiver"])
+
+receiver_value = get_receiver_totals_by_match(chains)
+receiver_value = receiver_value.rename(columns = {
+    "Receiver":"Player",
+    "xT_created":"xT_received",
+    "xT_denied":"xT_prevented",
+    "vaep_value":"vaep_value_received",
+    "exp_vaep_value":"exp_vaep_value_received"})
+
+player_stats = player_stats.merge(receiver_value[['Match_ID', 'Player', 'xT_received', 'xT_prevented', 'vaep_value_received', 'exp_vaep_value_received']], how = "left", on = ['Match_ID', 'Player'])
+
+player_stats.to_csv("/Users/ciaran/Documents/Projects/AFL/data/scored_player_stats.csv", index=False)
--- exploratory_analysis/merge_scored_chains.py
+++ exploratory_analysis/merge_scored_chains.py
@@ -10,11 +10,13 @@ def merge_and_export_scored_chains():
     chains = pd.read_csv(chain_file_path)
 
     threat_chains = pd.read_csv(xthreat_chain_output_path)
+    chains = chains.merge(threat_chains[['Match_ID', 'Chain_Number', 'Order', 'ReceiverTeam', 'Receiver', 'xT_created', 'xT_denied']], how = "left", on = ['Match_ID', 'Chain_Number', 'Order'])
+
     vaep_chains = pd.read_csv(vaep_chain_output_path)
     exp_vaep_chains = pd.read_csv(exp_vaep_chain_output_path)
 
     vaep_chains = vaep_chains.merge(exp_vaep_chains[['match_id', 'chain_number', 'order','xScore', 'exp_scores', 'exp_concedes', 'exp_offensive_value', 'exp_defensive_value', "exp_vaep_value"]], how = "left", on=['match_id', 'chain_number', 'order'])
-    chains = chains.merge(vaep_chains[['match_id', 'chain_number', 'order', "overall_seconds", 'action_type', 'outcome_type', 'scores', 'concedes', 'offensive_value', 'defensive_value', "vaep_value", 'exp_scores', 'exp_concedes', 'exp_offensive_value', 'exp_defensive_value', "exp_vaep_value"]], how = "left", left_on = ['Match_ID', 'Chain_Number', 'Order'], right_on=['match_id', 'chain_number', 'order'])
+    chains = chains.merge(vaep_chains[['match_id', 'chain_number', 'order', "overall_seconds", 'action_type', 'outcome_type', 'scores', 'concedes', 'offensive_value', 'defensive_value', "vaep_value", 'xScore', 'exp_scores', 'exp_concedes', 'exp_offensive_value', 'exp_defensive_value', "exp_vaep_value"]], how = "left", left_on = ['Match_ID', 'Chain_Number', 'Order'], right_on=['match_id', 'chain_number', 'order'])
 
     chains.to_csv(scored_chain_file_path, index=False)
     
